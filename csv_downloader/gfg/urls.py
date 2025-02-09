1ac435895c3bdbf7c13face70d0a4f893d2691b1
rathodkinjalba <rathodkinjalba4462@gmail.com>
2025-02-09 17:59:44 +0530

    [ADD] Added CSV Exporter module

---
+++ csv_downloader/gfg/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path 
+from . import views 
+  
+urlpatterns = [ 
+    path('', views.home, name='home'), 
+    path('generate-csv/', views.generate_csv, name='generate_csv'), 
+] 
